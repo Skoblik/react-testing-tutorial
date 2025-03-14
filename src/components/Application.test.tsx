1500a57c7d862811b09476316aa14bf1997654c6
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-14 10:45:24 +0200

    Application component test updated

--- src/components/Application.test.tsx
+++ src/components/Application.test.tsx
@@ -1,24 +1,41 @@
-import { render, screen } from '@testing-library/react'
-import { Application } from './Application'
+import { render, screen } from "@testing-library/react";
+import { Application } from "./Application";
 
-describe('Application', () => {
-    test('renders the application form', () => {
-        render(<Application />)
+describe("Application component test", () => {
+  test("renders the application form heading", () => {
+    render(<Application />);
 
-        const heading = screen.getByRole('heading', { name: /job application form/i })
-        expect(heading).toBeInTheDocument()
-        }),
-    test('renders the custom HTML element', () => {
-        render(<Application />)
+    const heading = screen.getByRole("heading", {
+      name: /job application form/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
 
-        const customElement = screen.getByTestId('custom-element')
-        expect(customElement).toBeInTheDocument()
-        expect(customElement).toHaveTextContent('Custom HTML element')
-       }),
-    test('verify text of the custom HTML element', () => {
-        render(<Application />)
+  test("renders the Section heading", () => {
+    render(<Application />);
+    const heading = screen.getByRole("heading", {
+      level: 2,
+      name: /section 1/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
 
-        const customElement = screen.getByTestId('custom-element')
-        expect(customElement).toHaveTextContent('Custom HTML element')
-       })         
-    })
+  test("verify the image is rendered", () => {
+    render(<Application />);
+
+    const image = screen.getByAltText("a mountain view");
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      "src",
+      "https://www.w3schools.com/images/picture.jpg"
+    );
+  });
+
+  test("renders the custom HTML element", () => {
+    render(<Application />);
+
+    const customElement = screen.getByTestId("custom-element");
+    expect(customElement).toBeInTheDocument();
+    expect(customElement).toHaveTextContent(/^Custom HTML element$/);
+  });
+});
