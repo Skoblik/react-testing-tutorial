f18be602241ac06399d1cd72d72bb85b9fc75706
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-23 16:17:06 +0200

    Added rendres a count of 10 after clicking the set button test

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -44,17 +44,16 @@ describe("Counter", () => {
     expect(countElement).toHaveTextContent("2");
   });
 
-  // test("rendres a count of 10 after clicking the set button", async () => {
-  //   user.setup()
-  //   render(<Counter />);
-  //   const amountInput = screen.getByRole("spinbutton");
-  //   await user.type(amountInput, "10");
-  //   expect(amountInput).toHaveValue(10);
-  //   const setButton = screen.getByRole("button", { name: "Set" });
-  //   await user.click(setButton);
-  //   const countElement = screen.getByRole("heading");
-  //   expect(countElement).toHaveTextContent("10");
-  // });
+  test("rendres a count of 10 after clicking the set button", async () => {
+    render(<Counter />);
+    const amountInput = screen.getByRole("spinbutton");
+    await user.type(amountInput, "10");
+    expect(amountInput).toHaveValue(10);
+    const setButton = screen.getByRole("button", { name: "Set" });
+    await user.click(setButton);
+    const countElement = screen.getByRole("heading");
+    expect(countElement).toHaveTextContent("10");
+  });
 
   // test("elements are focused in the right order", async () => {
   //   user.setup()
