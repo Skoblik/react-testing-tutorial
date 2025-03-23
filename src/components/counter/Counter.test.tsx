06ea8f39a3903285e6620551da0a546af9735e32
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-23 16:27:58 +0200

    Commented elements are focused in the right order test

--- src/components/counter/Counter.test.tsx
+++ src/components/counter/Counter.test.tsx
@@ -38,8 +38,7 @@ describe("Counter", () => {
   test("renders a count of 2 after clicking the increment button twice", async () => {
     render(<Counter />);
     const incrementButton = screen.getByRole("button", { name: "Increment" });
-    await user.click(incrementButton);
-    await user.click(incrementButton);
+    await user.dblClick(incrementButton);
     const countElement = screen.getByRole("heading");
     expect(countElement).toHaveTextContent("2");
   });
@@ -55,16 +54,16 @@ describe("Counter", () => {
     expect(countElement).toHaveTextContent("10");
   });
 
-  test("elements are focused in the right order", async () => {
-    render(<Counter />);
-    const amountInput = screen.getByRole("spinbutton");
-    const setButton = screen.getByRole("button", { name: "Set" });
-    const incrementButton = screen.getByRole("button", { name: "Increment" });
-    await user.tab();
-    expect(incrementButton).toHaveFocus();
-    await user.tab();
-    expect(amountInput).toHaveFocus();
-    await user.tab();
-    expect(setButton).toHaveFocus();
-  });
+  // test("elements are focused in the right order", async () => {
+  //   render(<Counter />);
+  //   const amountInput = screen.getByRole("spinbutton");
+  //   const setButton = screen.getByRole("button", { name: "Set" });
+  //   const incrementButton = screen.getByRole("button", { name: "Increment" });
+  //   await user.tab();
+  //   expect(incrementButton).toHaveFocus();
+  //   await user.tab();
+  //   expect(amountInput).toHaveFocus();
+  //   await user.tab();
+  //   expect(setButton).toHaveFocus();
+  // });
 });
