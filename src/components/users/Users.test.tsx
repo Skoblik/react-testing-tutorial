b236a796a948430321a4c3b0049bed068fb2b811
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-26 19:32:40 +0200

    Added renders an error message test

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -3,15 +3,21 @@ import { Users } from "./Users";
 
 describe("Users Component", () => {
   test("renders correctly", async () => {
-    render(<Users />);
+    render(<Users isFailRequest={false} />);
     const textElement = screen.getByRole("heading", { name: "Users" });
     expect(textElement).toBeInTheDocument();
     //await screen.findByText("Leanne Graham");
   });
 
   test("renders a list of users", async () => {
-    render(<Users />);
+    render(<Users isFailRequest={false} />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(10);
+  });
+
+  test("renders an error message", async () => {
+    render(<Users isFailRequest={true} />);
+    const errorMessage = await screen.findByText("Error fetching users");
+    expect(errorMessage).toBeInTheDocument();
   });
 });
